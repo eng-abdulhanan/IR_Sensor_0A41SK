c0e895416b7d9ebe5a8aed0f9683d084ccc059c1
eng-abdulhanan <57168597+eng-abdulhanan@users.noreply.github.com>
2019-11-07 15:03:35 +0500

    Add files via upload

---
+++ IR_Sensor.ino
@@ -0,0 +1,17 @@
+#define sensor A0 
+
+void setup() {
+  Serial.begin(9600); // start the serial port
+}
+
+void loop() {
+  
+
+  float volts = analogRead(sensor)*0.0048828125;  // value from sensor * (5/1024)
+  int distance = 13*pow(volts, -1); 
+  delay(1000); // slow down serial port 
+  
+  if (distance <= 30){
+    Serial.println(distance);  
+  }
+}
